2c1e4b6c0a6abbc94ea421f4fdcaf8bcf8a3297f
Femi-Benjamin <benjaminolufemi16@gmail.com>
2022-07-25 22:03:46 -0700

    made some changes to the Body and Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ export default function Footer() {
         <footer className='absolute bg-[#08091A] w-screen top-[1795px]'>
         <div className='not-italics font-Raleway text-white text-[28px] font-[700] leading-[53px] text-center m-[60px]'>
             <h1>Cycle Band</h1>
-            <p>Copy Right {new Date().getFullYear()}</p>
+            <p>Copy Right Ⓒ {new Date().getFullYear()}</p>
         </div>
         </footer>
     </div>
